70f5f2ff87d5ee2412770d3a9055e32beb3fba7b
GAVING0TGAME423 <78512752+GAVING0TGAME423@users.noreply.github.com>
2021-04-17 16:28:41 -0700

    Initial commit

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    Rigidbody RBPlayer;
+    public float speed = 10;
+    private GameObject FocalPoint;
+    Renderer rendererplayer;
+
+    void Start()
+    {
+       RBPlayer = GetComponent<Rigidbody>();
+        FocalPoint = GameObject.Find ("Focal Point");
+        rendererplayer = GetComponent<Renderer>();
+    }
+
+    
+    void Update()
+    {
+        float ForwardInput = Input.GetAxis("Vertical");
+        float magnitude = ForwardInput * speed * Time.deltaTime;
+        RBPlayer.AddForce(FocalPoint.transform.forward * magnitude , ForceMode.Impulse);
+
+        if(ForwardInput > 0)
+        {
+            rendererplayer.material.color = new Color(1 - ForwardInput, 1, 1 - ForwardInput);
+        }
+       else
+        {
+            rendererplayer.material.color = new Color(1 + ForwardInput, 1, 1 + ForwardInput);
+        }
+    }
+}
---
+++ Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    public float speed = 10;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        float RotateInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, RotateInput * speed * Time.deltaTime);
+    }
+}
