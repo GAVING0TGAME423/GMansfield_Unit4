94eb669ca62b8e1af8cbf1c14cfe7508c21d2ccf
GAVING0TGAME423 <78512752+GAVING0TGAME423@users.noreply.github.com>
2021-04-17 17:25:43 -0700

    GMansfield_Unit4-LessonB

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    Rigidbody Enemyrb;
+    GameObject player;
+    public float speed = 4;
+
+    void Start()
+    {
+        player = GameObject.Find("Player");
+       Enemyrb = GetComponent<Rigidbody>();
+    }
+
+    
+    void Update()
+    {
+        Vector3 SeekDirection = (player.transform.position - transform.position).normalized;
+        Enemyrb.AddForce(SeekDirection * speed * Time.deltaTime, ForceMode.Impulse);
+    }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject Enemyprefab;
+    private float spawnrange = 8.5f;
+    void Start()
+    {
+       
+        Instantiate(Enemyprefab, GenerateSpawnPos(), Enemyprefab.transform.rotation);
+    }
+
+   Vector3 GenerateSpawnPos()
+    {
+        float xPos = Random.Range(-spawnrange, spawnrange);
+        float zPos = Random.Range(-spawnrange, spawnrange);
+        Vector3 spawnposition = new Vector3(xPos, Enemyprefab.transform.position.y, zPos);
+        return spawnposition;
+    }
+   
+}
